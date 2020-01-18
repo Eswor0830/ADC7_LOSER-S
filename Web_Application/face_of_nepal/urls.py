9068d94bd6df7fb27f3f2511dceacbaf286a347b
SagarShresthaNP03A180082 <np03a180082@heraldcollege.edu.np>
2020-01-18 23:04:47 +0545

    update

---
+++ Web_Application/face_of_nepal/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+from . import views
+
+
+urlpatterns = [
+    path('hello/',view_hello_world),
+    path('', views.freelancer_form,name='freelancer_insert'), # get and post req. for insert operation
+    path('<int:id>/', views.freelancer_form,name='freelancer_update'), # get and post req. for update operation
+    path('delete/<int:id>/', views.freelancer_delete,name='freelancer_delete'),
+    path('list/', views.freelancer_list,name='freelancer_list') # get req. to retrieve and display all records
+
+
+
+]
--- Web_Application/face_of_nepal/views.py
+++ Web_Application/face_of_nepal/views.py
@@ -1,3 +1,40 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render
 
 # Create your views here.
+=======
+from django.shortcuts import render, redirect
+from .forms import FreelancerForm
+from .models import Freelancer
+
+
+# Create your views here.
+def view_hello_world(request):
+    return render(request,"index.html")
+
+
+def freelancer_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            form = FreelancerForm()
+        else:
+            freelancer = Freelancer.objects.get(pk=id)
+            form = FreelancerForm(instance=freelancer)
+        return render(request, "./freelancer_form.html", {'form': form})
+    else:
+        if id == 0:
+            form = FreelancerForm(request.POST)
+        else:
+            freelancer = Freelancer.objects.get(pk=id)
+            form = FreelancerForm(request.POST,instance= freelancer)
+        if form.is_valid():
+            form.save()
+        return redirect('/freelancer/list')
+
+
+def freelancer_delete(request,id):
+    freelancer = Freelancer.objects.get(pk=id)
+    freelancer.delete()
+    return redirect('/freelancer/list')
+
+>>>>>>> Stashed changes
